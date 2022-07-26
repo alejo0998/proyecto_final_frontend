46357d205cbae48248073fcded0a27c7e3bd77cf
alejo0998 <alejobusso09@gmail.com>
2022-07-26 03:32:56 -0300

    Avances integracion google

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+import gAuthPlugin from 'vue3-google-oauth2';
+let gauthClientId = '228465296603-p0v9a1bj4rasrgsolq03gmo1rgu8cakr.apps.googleusercontent.com';
+app.use(gAuthPlugin, {
+  clientId: gauthClientId,
+  scope: 'email',
+  prompt: 'consent',
+})
+
+app.mount('#app')
